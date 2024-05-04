c06b740189adc2b55a2a99e159cfbe54d0b4163b
Nazibulbd <mdnazibulislamnoyon2023@gmail.com>
2024-05-04 09:12:34 +0600

    site updated

--- app.js
+++ app.js
@@ -3,28 +3,232 @@ const addNewBtn = document.querySelector(".row button");
 const todoList = document.querySelector(".todo_list");
 
 
-addNewBtn.addEventListener("click", ()=>{
+// add new tasks
+function addNewTask(taskName) {
+    const li = document.createElement("li");
+    li.innerHTML = `<span id='taskName'>${taskName}</span>
+                    <span id="action">
+                      <i id="edit" class="fa-solid fa-pen"></i>
+                      <i id="delete" class="fa-solid fa-trash"></i>
+                    </span>`;
+  
+    todoList.append(li);
+    inputField.value = "";
+  }
+
+  
+// new task add handler
+function inputHandler(e) {
     const taskName = inputField.value;
-    if (taskName){
-        const li = document.createElement("li");
-        li.innerHTML = `${taskName} <i id="delete" class="fa-solid fa-trash"></i>`;
-        todoList.append(li);
-        inputField.value = ""
+    if (taskName) {
+      addNewTask(taskName);
+      addToLocalStorage(taskName);
     }
-});
-
-todoList.addEventListener("click", (e)=>{
+  }
+  
+  inputField.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
+      inputHandler(e);
+    }
+  });
+  
+  addNewBtn.addEventListener("click", inputHandler);
+  
+  function createTask(task) {
+    return `<span id='taskName'>${task}</span>
+    <span id="action">
+    <i id="edit" class="fa-solid fa-pen"></i>
+    <i id="delete" class="fa-solid fa-trash"></i>
+  </span>`;
+  }
+  
+//   delete function start
+  todoList.addEventListener("click", (e) => {
     const targetEl = e.target;
-    if(targetEl.id === "delete"){
-        targetEl.parentElement.remove()
+    if (targetEl.id === "delete") {
+      const li = targetEl.parentElement.parentElement;
+      const tasksName = li.querySelector("#taskName").textContent;
+      li.remove();
+      deleteTask(tasksName);
     }
-});
+//delete function end
+  
+//Edit function start
+    if (targetEl.id === "edit") {
+      const li = targetEl.parentElement.parentElement;
+      const preVal = li.querySelector("#taskName").textContent;
+  
+      const inp = document.createElement("input");
+      const uBtn = document.createElement("button");
+      uBtn.textContent = "U";
+    // uBtn.innerHTML = `${taskName} <i id="edit" class="fa-solid fa-pen"></i>`
+  
+      function updateHandler() {
+        const newVal = inp.value;
+        editTask(preVal, newVal);
+        const innerHTML = createTask(newVal);
+        li.innerHTML = innerHTML;
+      }
+    
+      uBtn.addEventListener("click", updateHandler);
+  
+      inp.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+          updateHandler();
+        }
+      });
+  
+      inp.value = preVal;
+      li.innerHTML = "";
+  
+      li.appendChild(inp);
+      li.appendChild(uBtn);
+    }
+  });
+  
+//Edit করে update  করার  function start
+  function addToLocalStorage(taskName) {
+    const tasks = loadTasksFromLocalStorage();
+    tasks.push(taskName);
+    addTasksToLocalStorage(tasks);
+  }
+  
+  // load task from local storage
+  function loadTasksFromLocalStorage() {
+    let tasks = [];
+    let rawTasks = localStorage.getItem("tasks");
+    if (rawTasks) {
+      tasks = JSON.parse(rawTasks);
+    }
+    return tasks;
+  }
+  
+  // load all tasks
+  function loadTasksToUI() {
+    const tasks = loadTasksFromLocalStorage();
+    tasks.forEach((taskName) => {
+      addNewTask(taskName);
+    });
+  }
+  
+  loadTasksToUI();
+  
+  function deleteTask(taskName) {
+    const tasks = loadTasksFromLocalStorage();
+    const tasksAfterDeleting = tasks.filter((task) => task !== taskName);
+    addTasksToLocalStorage(tasksAfterDeleting);
+  }
+  
+  function editTask(taskName, newTaskName) {
+    const tasks = loadTasksFromLocalStorage();
+    const tasksAfterEditing = tasks.map((task) => {
+      if (taskName === task) {
+        return newTaskName;
+      } else {
+        return task;
+      }
+    });
+    addTasksToLocalStorage(tasksAfterEditing);
+  }
+  
+  function addTasksToLocalStorage(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+  
 
-// listContainer.addEventListener("click", (e)=>{
-//     if(e.target.tagName = "LI"){
-//         e.target.classList.toggle("checked");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// addNewBtn.addEventListener("click", ()=>{
+//     const taskName = inputField.value;
+//     if (taskName){
+//         const li = document.createElement("li");
+//         li.innerHTML = `${taskName} <i id="delete" class="fa-solid fa-trash"></i>`;
+//         // li.innerHTML = `${taskName} <button id="delete">X</button>`;
+//         todoList.append(li);
+//         inputField.value = ""
 //     }
-//     else if(e.target.tagName === "I"){
-//         e.target.parentElement.remove
+// });
+
+// todoList.addEventListener("click", (e)=>{
+//     const targetEl = e.target;
+//     if(targetEl.id === "delete"){
+//         targetEl.parentElement.remove()
 //     }
-// }, false);
+// });
